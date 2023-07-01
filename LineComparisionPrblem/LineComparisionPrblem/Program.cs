2290071558f302054f768e4fac3b8f54a4558905
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-07-01 11:43:44 +0530

    [Kiran] Add\ find the lenght of line

---
+++ LineComparisionPrblem/LineComparisionPrblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionPrblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "Welcome to the Line Comaprision Problem");
+            LineComparision lineComparision = new LineComparision();
+            lineComparision.DisplayResult();
+        }
+    }
+}
